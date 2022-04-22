149da29d947b9d993420605391019697416f95a8
Tommy Holt <tommy_holt@icloud.com>
2022-04-22 16:58:08 +0100

    Updates

--- src/components/Meme/index.js
+++ src/components/Meme/index.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import memesData from "./memesData";
 
 export default function Header() {
-   function handleClick() {
-      console.log("swag");
-   }
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: ""
+  });
+  const [allMemeImages, setAllMemeImages] = useState([...memesData.data.memes])
+
+  function getRandomImage() {
+    const randomNumber = Math.floor(Math.random() * allMemeImages.length);
+    const url = allMemeImages[randomNumber].url
+    setMeme(prev => ({
+      ...prev,
+      randomImage: url
+    }));
+  }
 
   return (
     <main className="main">
       <div className="main--input-container">
         <input className="main--input-field" />
         <input className="main--input-field" />
+        <button className="main--button" onClick={getRandomImage}>
+          🤪 Generate new meme 🤪
+        </button>
       </div>
-      <button className="main--button" onClick={handleClick}>🤪 Generate new meme 🤪</button>
-      <div className="main--meme-container">
-        <img className="main--meme" />
-      </div>
+      <img className="main--meme" src={meme.randomImage} alt="A meme"/>
     </main>
   );
 }
